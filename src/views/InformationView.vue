0b95f95ee3a86b059aba4d266c6684cd7735d891
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-03-13 11:20:25 +0100

    FrontEnd & Respocive of Help page

--- src/views/InformationView.vue
+++ src/views/InformationView.vue
@@ -1,9 +1,32 @@
 <template>
-  <div class="informations">
-    <h1>This is the Help page</h1>
-    <router-link :to="{name : 'QuestionsView'}">Go</router-link> 
+  <div class="HolderOfHelp">
+    
+    <div class="informations">
 
+      <div class="Description">
+        <h2>This instruction will give you a hint how the test goes</h2>
+      </div>
+
+      <div class="instruction">
+        <p><b>[</b> If You didn't answer the question in 30s , it will consider as a fault answer <b>]</b></p>
+        <br>
+        <p><b>[</b> You Can't skip the question , until you choose one or the time end.<b>]</b></p>
+        <br>
+        <p><b>[</b> You will see your results only if you finished all the questions.<b>]</b></p>
+        <br>
+        <p><b>[</b> You will notice some circles in the top when you are passing the test. <br>Those Circles indicate how much question left.<b>]</b></p>
+        <br>
+        <p><b>[ Hint :</b>The Restart Button will delete your results . <br> If you want to keep it ! Then send it to your email<b>]</b></p>
+        <br>
+        <p><b>[ Warning :</b>Please do not open inspect or go to another page while passing the test. <br> It Will consider as cheating<b>]</b></p>
+      </div>
+
+      <router-link :to="{name : 'QuestionsView'}">Go</router-link> 
+
+    </div>
   </div>
+ 
+  
 </template>
 
 <script>
@@ -12,6 +35,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  @use "@/assets/scss/views/_help.scss";
 
 </style>
